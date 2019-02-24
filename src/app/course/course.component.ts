9658a691f116ca02db7b66dd1b54291aaa137715
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-24 12:49:56 +0300

    Material Data Table Server Side Pagination

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { LessonsDatasource } from '../services/lessons.datasource';
+import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'course',
@@ -19,18 +20,26 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   displayedColumns = ['seqNo', 'description', 'duration'];
 
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+
   constructor(
     private route: ActivatedRoute,
     private coursesService: CoursesService) { }
 
   ngOnInit() {
     this.course = this.route.snapshot.data.course;
-
     this.dataSource = new LessonsDatasource(this.coursesService);
-
-    this.dataSource.loadLessons(this.course.id, '', 'asc', 0, 3);
   }
 
-  ngAfterViewInit() { }
+  ngAfterViewInit() {
+    this.paginator.page
+      .pipe(
+        startWith(null) // to trigger the call to load lessons
+      )
+      .subscribe(() => {
+        this.dataSource.loadLessons(this.course.id, '', 'asc', this.paginator.pageIndex, this.paginator.pageSize);
+      });
+  }
 
 }
